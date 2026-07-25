37ad3280882cca6b5589b8ff7535e7ac1b008387
Sachin Chaudhary <sachinchuadhary45@gmail.com>
2026-07-25 23:11:31 +0530

    Update NotificationService.java

--- src/main/java/com/project/moneymanager/service/NotificationService.java
+++ src/main/java/com/project/moneymanager/service/NotificationService.java
@@ -39,7 +39,7 @@ public class NotificationService {
     }
 
     // @Scheduled(cron = "0 0 23 * * *", zone = "IST")
-    @Scheduled(cron = "0 11 11 * * *", zone = "IST")
+    @Scheduled(cron = "0 11 13 * * *", zone = "IST")
     public void sendDailyExpenseSummary(){
         log.info("Job started: sendDailyExpenseSummary()");
         List<ProfileEntity> profiles = profileRepository.findAll();
